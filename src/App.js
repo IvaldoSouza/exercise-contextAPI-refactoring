3c108614e9fa08b55a3c24406c5e18a344544f40
Ivaldo Souza <souza1976ivaldo@gmail.com>
2021-07-22 11:34:21 -0300

    exercício concluído

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import React from 'react';
 import './App.css';
 import Cars from './Cars';
+import carsContext from './contextApi/carsContext';
 
-function App() {
-  return (
-    <Cars />
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cars: {
+        red: false,
+        blue: false,
+        yellow: false,
+      },
+    }
+    this.moveCar = this.moveCar.bind(this);
+  }
+
+  moveCar(car, side) {
+    this.setState({
+      cars: {
+        ...this.state.cars,
+        [car]: side,
+      },
+    });
+  };
+
+  render() {
+
+    const context = {
+      ...this.state,
+      moveCar: this.moveCar,
+    };
+
+    return (
+      <carsContext.Provider value={ context }>
+        <Cars />
+      </carsContext.Provider>
+    );
+  }
 }
 
 export default App;
